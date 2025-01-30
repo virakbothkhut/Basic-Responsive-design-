110ed4d9f9187062c559537bc147abfdfb9551cb
virakbothkhut <virakbothkhut@gmail.com>
2025-01-30 13:20:49 +0100

    update

---
+++ web_responsive_II/script.js
@@ -0,0 +1,7 @@
+// Function to toggle the menu visibility
+function myFunction() {
+  var x = document.getElementById("main-menu");
+
+  // Toggle the 'active' class to show/hide the menu
+  x.classList.toggle("active");
+}
